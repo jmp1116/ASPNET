85a40ac3beb0cd6fd42c987f027ce3b308dc3eac
John Mark <jmp1116@gmail.com>
2020-12-30 18:48:12 -0600

    finished ASP.NET project

---
+++ IProductRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<ProductModel> GetAllProducts();
+        public ProductModel GetProduct(int id);
+        public void UpdateProduct(ProductModel product);
+        public void InsertProduct(ProductModel productToInsert);
+        public IEnumerable<CategoryModel> GetCategories();
+        public ProductModel AssignCategory();
+        public void DeleteProduct(ProductModel product);
+    }
+   
+
+}
